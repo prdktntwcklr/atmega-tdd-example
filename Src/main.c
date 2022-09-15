85a3bcc5826f0b7a5809d401117a8fa3bf6fd9ee
prdktntwcklr <61001903+prdktntwcklr@users.noreply.github.com>
2022-09-15 21:23:18 +0800

    add helper functions to main

--- Src/main.c
+++ Src/main.c
@@ -4,15 +4,30 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+static void led_init(void);
+static void led_toggle(void);
+
 int main(void)
 {
-	DDRD |= (1 << 3);
+	led_init();
 	
 	while(1)
 	{
-		PORTD ^= (1 << 3);
+		led_toggle();
 		_delay_ms(1000);
 	}
 	
 	return 0;
+}
+
+static void led_init(void)
+{
+	/* configure LED pin as output */
+	DDRD |= (1 << DDD3);
+}
+
+static void led_toggle(void)
+{
+	/* toggle LED pin */
+	PORTD ^= (1 << PD3);
 }
