a443f5db617ebf91ebd31df21ff53d9bc1d30874
prdktntwcklr <61001903+prdktntwcklr@users.noreply.github.com>
2022-09-21 19:59:49 +0800

    add sleep mode to main

--- Src/main.c
+++ Src/main.c
@@ -2,6 +2,8 @@
 
 #include "main.h"
 #include "superloop.h"
+#include "avr/interrupt.h"
+#include "avr/sleep.h"
 
 #ifndef TEST
 int main(void)
@@ -10,10 +12,20 @@ int testable_main(void)
 #endif
 {
 	superloop_init();
+	set_sleep_mode(SLEEP_MODE_IDLE);
 	
 	while(superloop_run())
 	{
-		// TODO: enter power saving mode here
+		/* send mcu to sleep, sequence recommended in avr/sleep.h */
+		cli();
+		if(true)
+		{
+			sleep_enable();
+			sei();
+			sleep_cpu();
+			sleep_disable();
+		}
+		sei();
 	}
 	
 	return 0;
