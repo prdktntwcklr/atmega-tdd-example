55a3bd71a10f3230c4667a1f71134c1f2035c729
prdktntwcklr <61001903+prdktntwcklr@users.noreply.github.com>
2026-01-31 11:33:50 +0000

    start add coverage

--- unit-tests.sh
+++ unit-tests.sh
@@ -7,5 +7,4 @@ build_dir=Build
 rm -rf $build_dir/
 mkdir -p $build_dir/
 cmake -DTARGET_GROUP=test -S . -B $build_dir/
-cmake --build $build_dir/
-ctest --test-dir $build_dir/Tests/ -V
+cmake --build $build_dir/ --target run_tests
