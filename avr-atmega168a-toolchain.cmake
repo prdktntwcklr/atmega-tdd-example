547d2e242929442c4ef1ed807fb33732c0dd367c
prdktntwcklr <61001903+prdktntwcklr@users.noreply.github.com>
2026-01-31 10:31:49 +0000

    chore: clean up CMake files

--- avr-atmega168a-toolchain.cmake
+++ avr-atmega168a-toolchain.cmake
@@ -8,9 +8,6 @@ find_program(AVR_OBJCOPY avr-objcopy REQUIRED)
 find_program(AVR_SIZE avr-size REQUIRED)
 find_program(AVR_DUDE avrdude REQUIRED)
 
-# set name of executable
-set(EXECUTABLE App)
-
 # define mandatory variables
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR avr)
@@ -22,19 +19,7 @@ set(AVR_PARTNO m168)
 set(AVR_PROGRAMMER avrispv2)
 set(F_CPU "1000000UL")
 
-# set compile options
-add_compile_options(
-    -Wall
-    -g
-    -Os
-    -mmcu=${AVR_MCU}
-    -DF_CPU=${F_CPU}
-    )
-
-# set linker options
-add_link_options(
-    -mmcu=${AVR_MCU}
-    )
+set(MCU_FLAGS "-mmcu=${AVR_MCU}")
 
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
